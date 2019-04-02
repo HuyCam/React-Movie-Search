68d550db7added7f3fa5bf7fb3e9c9eaa7d2eeef
HuyCam <camhuy165@gmail.com>
2019-04-02 00:05:10 -0500

    Update

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,11 +3,14 @@ export const SEARCH_MOVIE = 'SEARCH_MOVIE';
 export function search(term) {
     console.log('Action invoke', term);
 
+<<<<<<< HEAD
     const URL = `http://www.omdbapi.com/?apikey=2905f329&t=${term}`;
     const request = axios.get(URL);
     console.log('Action invoke', URL);
+=======
+>>>>>>> parent of 8f7348c... Update Movie Detail
     return {
         type: SEARCH_MOVIE,
-        payload: request
+        payload: term
     };
 }
--- src/containers/movie_detail.js
+++ src/containers/movie_detail.js
@@ -4,6 +4,16 @@ import { connect } from 'react-redux';
 
 class MovieDetail extends Component {
     render() {
+        let movie;
+        if (!this.props.data) {
+            movie = this.props.data.data;
+        } else {
+            return <div>Movie</div>;
+        }
+
+        if (movie.Error) {
+            return <div>{movie.Error}</div>;
+        }
         return(
             <div className="container">
                 <div className="row">
@@ -11,7 +21,13 @@ class MovieDetail extends Component {
                         !MovieDetail
                     </div>
                     <div className="col-md-6">
-                        !MovieDetail
+                        <ul classsName="list-group" >
+                            <DetailSection detail={['Actor', movie.Actors]} />
+                            <DetailSection detail={['Director', movie.Director]} />
+                            <DetailSection detail={['Genre', movie.Genre]} />                        
+                            <DetailSection detail={['Production', movie.Production]} />
+                            <DetailSection detail={['Plot', movie.Plot]} />
+                      </ul>
                     </div>
                 </div>
             </div>
